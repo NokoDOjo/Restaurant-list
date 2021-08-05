d1f7849f511a4a9412bf829dc2c654c331c5670a
NokoDOjo <81893207+NokoDOjo@users.noreply.github.com>
2021-08-05 12:24:05 +0800

    feat: add user login feature via passport

--- app.js
+++ app.js
@@ -2,10 +2,10 @@
 // require packages used in the project
 const express = require('express')
 const methodOverride = require('method-override')
+const session = require('express-session')
+const usePassport = require('./config/passport')
 const app = express()
 const port = 3000
-const restaurantList = require('./restaurant.json')
-const Restaurant = require('./models/restaurant')  //載入restaurant model
 const exphbs = require('express-handlebars')
 
 const routes = require('./routes')
@@ -14,11 +14,19 @@ require('./config/mongoose')
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
+app.use(session({
+  secret: '12345',
+  resave: false,
+  saveUninitialized: true
+}))
+
 app.use(express.static('public'))
 // Body-parser
 app.use(express.urlencoded({ extended: true }))
 // Method override
 app.use(methodOverride('_method'))
+
+usePassport(app)
 // routes
 app.use(routes)
 
