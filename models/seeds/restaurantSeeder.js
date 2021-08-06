3af226ac6f3f29bb861a3aaebabd41f48ffcde41
NokoDOjo <81893207+NokoDOjo@users.noreply.github.com>
2021-08-06 15:32:52 +0800

    modify restaurantSeeder

--- models/seeds/restaurantSeeder.js
+++ models/seeds/restaurantSeeder.js
@@ -1,14 +1,34 @@
+const bcrypt = require('bcryptjs')
 const Restaurant = require('../restaurant')
 const restaurantList = require('../../restaurant.json').results
+const User = require('../user')
+const userList = require('../../user.json').results
 const db = require('../../config/mongoose')
 
 // 連線成功
 // 載入餐廳資料
 db.once('open', () => {
-  for (let i = 0; i < restaurantList.length; i++) {
-    Restaurant.create({ 
-      ...restaurantList[i]
-    })
-  }
-  console.log('mongodb connected! Restaurant data done !!')
+  userList.forEach( seedUser => {
+    bcrypt
+      .genSalt(10)
+      .then(salt => bcrypt.hash(seedUser.password, salt))
+      .then(hash =>
+        User.create({
+          name: seedUser.name,
+          email: seedUser.email,
+          password: hash
+        }))
+      .then(user => {
+        const userId = user._id
+        return Promise.all(Array.from(
+        seedUser.ownedRestaurantID, 
+        id => Restaurant.create({ ...restaurantList[id-1], userId })
+        ))
+      })
+      .then(() => {
+        console.log('done.')
+        process.exit()
+      })
+      .catch(error => console.log(error))
+  })
 })
