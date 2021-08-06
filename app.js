b805453ad8ab0aa8b84d10e27e669e1ce25b683a
NokoDOjo <81893207+NokoDOjo@users.noreply.github.com>
2021-08-06 11:49:00 +0800

    feat: add google & facebook login & dotenv

--- app.js
+++ app.js
@@ -7,16 +7,20 @@ const session = require('express-session')
 const usePassport = require('./config/passport')
 const flash = require('connect-flash')
 const app = express()
-const port = 3000
-
+const PORT = process.env.PORT || 3000
 const routes = require('./routes')
+
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
 require('./config/mongoose')
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
 app.use(session({
-  secret: '12345',
+  secret: process.env.SESSION_SECRET,
   resave: false,
   saveUninitialized: true
 }))
@@ -42,6 +46,6 @@ app.use((req, res, next) => {
 app.use(routes)
 
 // start and listen on the Express server
-app.listen(port, () => {
-  console.log(`Express is listening on http://localhost:${port}`)
+app.listen(PORT, () => {
+  console.log(`Express is listening on http://localhost:${PORT}`)
 })
